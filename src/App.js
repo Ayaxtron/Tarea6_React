1441b84ca798378258b9e3ad617554e134b4f8fd
Ayax <casarrubias2837@gmail.com>
2020-04-24 14:46:16 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Create from './components/todo/Create';
+import Index from './components/todo/Index';
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    { description: 'Create main folder', status: 'pending' }, 
+    { description: 'Move to main folder', status: 'pending' }, 
+    { description: 'Start npm in the folder', status: 'pending' }
+  ]);
+
+  const addTodo = (description) => {
+    let cTodos = Object.assign([], todos);
+    cTodos.push({ description: description, status: 'pending' });
+    setTodos(cTodos);
+  }
+
+  const markAsDone = (task) => {
+    let cTodos = Object.assign([], todos);
+    cTodos[task].status = 'done';
+    setTodos(cTodos);
+  }
+
+  const deleteTask = (task) => {
+    let cTodos = Object.assign([], todos);
+    cTodos.splice(task, 1);
+    setTodos(cTodos);
+  }
+
+  return (
+    <>
+      <h1>Todo list</h1>
+      <Create addTodo={addTodo}/>
+      <Index todos={todos} markAsDone={markAsDone} deleteTask={deleteTask} />
+    </>
+  );
+}
+
+export default App;
